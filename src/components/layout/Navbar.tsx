e52be48491e2209f72840cadf865a768efabb209
Abhinav <itsabhinav5383@gmail.com>
2025-02-23 17:41:19 +0530

    Fix icon path

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <header class="w-full flex items-center justify-between py-4 px-8 bg-card-background">
             <div class="flex items-center gap-x-2">
-                <img src="/src/assets/icon.png" alt="Logo" class="h-10 w-10" />
+                <img src="/icon.png" alt="Logo" class="h-10 w-10" />
                 <span class="text-lg-plus font-bold">CR translation tool</span>
             </div>
 
